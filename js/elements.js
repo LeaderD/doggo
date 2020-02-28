dae0322ad9f5626a5821baf4f713cc8bf8c2f1b3
kathleenhung5 <kathleenhung5@gmail.com>
2020-02-27 16:59:27 -0800

    styling cards

--- js/elements.js
+++ js/elements.js
@@ -1,11 +1,13 @@
 function Profile(dog) {
     return $(`
   <div class="row card" onclick="openSB(${dog.id})">
-        <img src="${dog.img}" class="responsive"/>
-        <p>${dog.name}</p>
-        <p>Age:${dog.age}</p>
-        <p>Breed: ${dog.breed}</p> 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        <div style="background-image:url(${dog.img})"><div>
+        <div class="dog-des">
+          <p>${dog.name}</p>
+          <p>Age:${dog.age}</p>
+          <p>Breed: ${dog.breed}</p> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        </div>
   </div>
     `)
 }
