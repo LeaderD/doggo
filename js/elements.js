14cff680dad8c8f208fecbca929b16f4c776f4d1
kathleenhung5 <kathleenhung5@gmail.com>
2020-02-27 20:35:33 -0800

    edit styling

--- js/elements.js
+++ js/elements.js
@@ -16,7 +16,7 @@ function Profile(dog) {
 function SideBar(dog){
     return $(`
 <a href="javascript:void(0)" class="closebtn" onclick="closeSB()">×</a>
-  <img src=" ${dog.img}" width="550">
+  <img src=" ${dog.img}" width="100%">
   <div class="sb-des">
     <h1 class="name_breed"> ${dog.name} - ${dog.breed} </h1>
     <h3>  ${dog.age} old • ${dog.gender} • ${dog.coat} • ${dog.size}</h3>
